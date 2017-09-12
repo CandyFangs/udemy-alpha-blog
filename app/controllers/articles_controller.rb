17295c464f55e1e1115085adcdaf879c3c2f5a39
Paulina Osiak <paulina.osiak@gmail.com>
2017-09-12 14:52:39 +0200

    Update cuttern user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
   def create
     debugger
     @article = Article.new(article_params)
-    @article_params.user = User.first
+    @article_params.user = current_user
     if @article.save
       flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
